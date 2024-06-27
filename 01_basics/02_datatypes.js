3956ed0d246b1200491402f10962d51050d69763
sumanmodak578 <151933074+sumanmodak578@users.noreply.github.com>
2024-06-27 15:32:01 +0000

    js datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    + 
+    3); // Code readability should be high
+console.log("Suman");
+
+
+let name = "Suman"
+let age = 24
+let isLoggedIn = true
+let state
+let a = null
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
