cbdac3c5b152bc399b4f747ebdee380d0b2190e2
sumanmodak578 <151933074+sumanmodak578@users.noreply.github.com>
2024-06-29 12:31:32 +0000

    datatypes in js

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive Data Type
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(id == anotherId);
+
+const bigNumber = 43848374374983n
+
+
+// Reference(Non Primitive) Type
+
+// Arrays, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let obj = {
+    name: "suman",
+    age: 23,
+}
+
+const myFunc = function(){
+    console.log("Hello World");
+}
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof outsideTemp);
+console.log(typeof id);
+console.log(typeof bigNumber);
+
+
+console.log(typeof heros); 
+console.log(typeof obj); 
+console.log(typeof myFunc); // This is a function object
