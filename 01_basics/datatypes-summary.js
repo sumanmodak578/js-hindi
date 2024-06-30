bb7e971a9fb08fedf8d6f091f49dfedfbf54d2c2
sumanmodak578 <151933074+sumanmodak578@users.noreply.github.com>
2024-06-30 15:04:36 +0000

    stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -31,13 +31,43 @@ let obj = {
 const myFunc = function(){
     console.log("Hello World");
 }
-console.log(typeof scoreValue);
-console.log(typeof isLoggedIn);
-console.log(typeof outsideTemp);
-console.log(typeof id);
-console.log(typeof bigNumber);
+// console.log(typeof scoreValue);
+// console.log(typeof isLoggedIn);
+// console.log(typeof outsideTemp);
+// console.log(typeof id);
+// console.log(typeof bigNumber);
 
 
-console.log(typeof heros); 
-console.log(typeof obj); 
-console.log(typeof myFunc); // This is a function object
+// console.log(typeof heros); 
+// console.log(typeof obj); 
+// console.log(typeof myFunc); // This is a function object
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack Memory(Primitive Data Types), Heap memory(Non-Primitve Data Types)
+
+
+// Call by Memory 
+
+let myEmailName = "suman@google.com"
+
+let anotherName = myEmailName
+anotherName = "modak@google.com"
+
+console.log(myEmailName);
+console.log(anotherName);
+
+
+// Call by Reference
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+userTwo.email = "example@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
