ae8083aa03f971512d00034bc82e62ce4afd4add
sumanmodak578 <151933074+sumanmodak578@users.noreply.github.com>
2024-07-05 13:02:03 +0000

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,10 +8,54 @@ if (true){
     // Block Scope
     let a = 10
     const b = 20
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "suman"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "suman"
+
+    if (username === "suman") {
+        const website = "youtube"
+        // console.log(username+website);
+    }
+
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++
+
+console.log(addOne(5));
+
+function addOne(num){  // functions
+    return num + 1
+}
+
+
+console.log(addTwo(5));
+
+const addTwo = function(num){ // Expressions
+    return num + 2
+}
