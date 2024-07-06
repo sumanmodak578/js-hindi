f221e94d99e29d22b3f8def1606c2d8f37a95253
sumanmodak578 <151933074+sumanmodak578@users.noreply.github.com>
2024-07-06 07:44:55 +0000

    while and do while in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while loop
+
+let index = 0
+
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index+=2
+// }
+
+let myArray = ['thor', 'hulk', 'groot', 'rocket']
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr+=1
+// }
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
